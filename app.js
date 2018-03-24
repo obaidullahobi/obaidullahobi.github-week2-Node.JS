dfede6081717ded8565a868a50423f4aaf247d81
obaidullahobi <33807026+obaidullahobi@users.noreply.github.com>
2018-03-24 07:48:35 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+
+/*setTimeout
+setTimeout(function(){
+console.log('4 second have passed');
+}, 4000);
+
+ */
+
+/*setInterval
+ var  time = 0;
+ setInterval(function(){
+     time += 2;
+    console.log( time + ' second have passed');
+    }, 2000);
+    */
+
+    /*clearinterval timer
+   var  time = 0;
+   var timer = setInterval(function(){
+       time += 2;
+      console.log( time + ' second have passed');
+      if(time > 5){
+          clearInterval(timer);
+      }
+      }, 2000);
+      */
+     
+      /*function expression
+      function callFunction(fun){
+          fun();
+      }
+
+      
+      var sayBye = function(){
+          console.log('bye');
+      };
+
+      callFunction(sayBye);
+      */
